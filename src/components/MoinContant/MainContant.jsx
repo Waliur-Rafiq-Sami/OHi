362c1255832c780719c7a2fb2737ecd0288d554e
Waliur Rafiq Sami <waliurrafiqsami@gmail.com>
2024-08-31 00:32:24 +0600

    tow sector not ready

--- src/components/MoinContant/MainContant.jsx
+++ src/components/MoinContant/MainContant.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const MainContant = ({ pic, color, ht, hcolor, text, inbox }) => {
+const MainContant = ({ pic, color, ht, hcolor, headerColor, text, inbox }) => {
   const [show, setShow] = useState(false);
   const onShow = () => {
     setShow(!show);
@@ -8,34 +8,38 @@ const MainContant = ({ pic, color, ht, hcolor, text, inbox }) => {
   return (
     <>
       <div
-        className="min-h-screen bg-no-repeat bg-blue-300 bg-fixed md:bg-contain bg-cover flex items-end max-w-[500px] relative "
+        className={`
+          min-h-screen bg-no-repeat  bg-blue-300 bg-fixed md:bg-contain bg-cover flex items-end max-w-[500px] relative 
+          `}
         style={{
           backgroundImage: `url(${pic})`,
         }}
       >
         <div
-          className={`container mx-auto p-5 font-bold text-xl text-justify ${color}text-[#2f50bb]`}
+          className={`container mx-auto p-5 font-bold text-xl text-justify `}
         >
-          <h1 className={`text-5xl ${hcolor}`}>{show ? <></> : <>{ht}</>}</h1>
-          <p className={` ${show ? "hidden" : ""}`}>
+          <h1 className={`text-5xl ${hcolor} text-[#88f1ff] py-5`}>
+            {show ? <></> : <>{ht}</>}
+          </h1>
+          <p className={` ${color} ${show ? "hidden" : ""}`}>
             {text}
             <button
               onClick={onShow}
-              className="btn text-lg btn-sm bg-[#9741b1] border-none text-transparent btn-info font-extrabold btn-outline font-2"
+              className="btn text-lg btn-sm bg-[#9741b1] border-none text-transparent btn-info font-extrabold btn-outline font-2 ml-2"
             >
               {"Read"}
             </button>
           </p>
-          <div className="h-screen backdrop-blur-sm mx-3 mb-10 absolute top-0 left-0 pb-8 transition-transform transform hover:scale-105">
+          <div className="h-screen backdrop-blur-sm m-5  mb-10 absolute top-0 left-0 pb-8 transition-transform transform hover:scale-105">
             <div
-              className={`bg-[#5670a844] rounded-xl my-3 p-3 overflow-x-auto h-full ${
+              className={` ${headerColor} rounded-xl my-3 p-3 overflow-x-auto h-full ${
                 show ? "" : "hidden"
               }`}
             >
-              <p className="inline-block">{inbox}</p>
+              <p className="inline-block pt-2 pb-10 px-1">{inbox}</p>
               <p className="text-end">
                 <button
-                  className="btn-warning btn mt-5 font-bold text-lg"
+                  className=" btn-warning btn mt-5 mb-10 font-bold text-lg"
                   onClick={onShow}
                 >
                   Close
